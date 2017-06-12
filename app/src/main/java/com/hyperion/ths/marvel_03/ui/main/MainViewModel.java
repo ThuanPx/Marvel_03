75a9932b1b9fb78dac00667d187fc1315f6fb29e
HyperionThS <mystogan.ths@gmail.com>
2017-06-12 14:43:01 +0700

    feature add check connection

--- app/src/main/java/com/hyperion/ths/marvel_03/ui/main/MainViewModel.java
+++ app/src/main/java/com/hyperion/ths/marvel_03/ui/main/MainViewModel.java
@@ -1,18 +1,39 @@
 package com.hyperion.ths.marvel_03.ui.main;
 
+import android.databinding.Bindable;
+import com.hyperion.ths.marvel_03.BR;
+import com.hyperion.ths.marvel_03.connection.MainReceiver;
 import com.hyperion.ths.marvel_03.ui.BaseViewModel;
 
 /**
  * Created by ths on 27/05/2017.
  */
 
-public class MainViewModel extends BaseViewModel {
+public class MainViewModel extends BaseViewModel implements NetworkChangeListener {
     private MainViewPageAdapter mMainViewPageAdapter;
-    public MainViewModel(MainViewPageAdapter mainViewPageAdapter) {
+    private boolean mIsConnected;
+
+    public MainViewModel(MainViewPageAdapter mainViewPageAdapter, MainReceiver mainReceiver) {
         mMainViewPageAdapter = mainViewPageAdapter;
+        mainReceiver.setNetworkChangeListener(this);
     }
 
     public MainViewPageAdapter getMainViewPageAdapter() {
         return mMainViewPageAdapter;
+    }
+
+    @Bindable
+    public boolean isConnected() {
+        return mIsConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        mIsConnected = connected;
+        notifyPropertyChanged(BR.connected);
+    }
+
+    @Override
+    public void onNetworkChange(boolean isConnected) {
+        setConnected(isConnected);
     }
 }
